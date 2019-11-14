e8411c9ff3a3bbda23284465203c33b7d7a5875e
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-11-14 13:09:47 +0300

    Add solution to eqVneq in hw05

--- homework/hw05.v
+++ homework/hw05.v
@@ -16,11 +16,11 @@ Print eq_op.
 Lemma eqVneq (T : eqType) (x y : T) :
   eq_xor_neq x y (y == x) (x == y).
 Proof.
-  case: (@eqP _ x y). Undo.
   rewrite eq_sym.
   case: eqP.
   move=>->.
-  constructor.
+  - by constructor.
+  - by constructor; apply/eqP.
 Qed.
 
 (* Use eqVneq to prove the following lemma.
